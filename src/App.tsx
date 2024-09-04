5217004e8e51b4eab483e6d6336db0c35521dd8a
misterdan100 <danielmca60@gmail.com>
2024-09-03 21:04:56 -0500

    feat: create form w/ number validation

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <header className="bg-blue-600 py-8 max-h-72">
-        <h1 className="uppercase text-center font-black text-4xl text-white">
+      <header className="bg-sky-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white italic">
           Expenses Planner
         </h1>
       </header>
--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,6 +1,39 @@
+import { useMemo, useState } from "react"
 
 export default function BudgetForm() {
+    const [budget, setBudget] = useState(0)
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setBudget(e.target.valueAsNumber)
+    }
+
+    const isValid = useMemo(() => {
+        return isNaN(budget) || budget <= 0
+    }, [budget])
+
   return (
-    <div>BudgetForm</div>
+    <form className="space-y-5">
+        <div className="flex flex-col space-y-5">
+            <label htmlFor="budget" className="text-4xl text-sky-600 font-bold text-center">
+                Define Budget
+            </label>
+            <input 
+                id="budget"
+                type="number" 
+                className="w-full bg-white border border-sky-200 p-2 text-center rounded-2xl"
+                placeholder="Enter your budget"
+                name="budget"
+                value={budget}
+                onChange={handleChange}
+            />
+        </div>
+
+        <input 
+            type="submit" 
+            value='Define Budget'
+            className="bg-sky-600 hover:bg-sky-700 cursor-pointer w-full p-2 text-white font-black uppercase rounded-2xl disabled:opacity-40"
+            disabled={isValid}
+        />
+    </form>
   )
 }
